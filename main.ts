e757adb17234ee05412138a551367f8295ad80c2
oriolcorbella <154092801+oriolcorbella@users.noreply.github.com>
2025-12-09 16:46:10 +0100

    Actualització pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
 basic.forever(function () {
-	
+    if (input.temperature() > 22) {
+        basic.showString("CALOR")
+        music.play(music.stringPlayable("B A G A G F A C5 ", 400), music.PlaybackMode.UntilDone)
+    } else {
+        basic.showString("FRED")
+        music.play(music.stringPlayable("C5 B A B C5 B A B ", 90), music.PlaybackMode.UntilDone)
+    }
 })
